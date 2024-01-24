96e3a95d9f5a6992a3a8f101bdb5dfd311aefbbb
RaccoonAstrologer <z.niklal@gmail.com>
2024-01-24 17:50:00 +0200

    hw2

---
+++ Homework2.py
@@ -0,0 +1,9 @@
+import random
+def get_numbers_ticket(min, max, quantity):
+    for num in (min, max, quantity):
+        if not (1 <= num <= 1000):
+            return
+        uniq_numbs = random.sample(range(min, max + 1), quantity)
+        return sorted(uniq_numbs)
+lottery_numbers = get_numbers_ticket(1,49, 6)
+print('Ваші лотерейні числа: ', lottery_numbers)
