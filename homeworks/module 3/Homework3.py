c235e06fab4d721b86a5babb8cafe023abac4e51
RaccoonAstrologer <z.niklal@gmail.com>
2024-01-25 14:16:28 +0200

    hw

--- homeworks/module 3/Homework3.py
+++ homeworks/module 3/Homework3.py
@@ -1,19 +1,29 @@
-from datetime import datetime
-def get_days_from_today(date):
-       while True:    
-           try:
-                 converted_date = datetime.strptime(date, "%Y-%m-%d")
-                 current_date = datetime.today()            
-                 difference  = converted_date.toordinal() - current_date.toordinal()
-                 return difference
-           except ValueError:
-                 print("Невірний формат вводу")
-                 date = input("Введість дату в форматі yyyy-mm-dd: ")
-                 continue
-           else:
-                 break
+import re
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+dirty_phones=["    +38(050)123-32-34",
+"     0503451234",
+"(050)8889900",
+"38050-111-22-22",
+"38050 111 22 11   "]
+def normalize_phone(phone_number):
+    p1=r"[\d\+]+"
+    phone_number=''.join(re.findall(p1,phone_number))
+    if len(phone_number)==10:
+        phone_number='+38'+phone_number
+    elif len(phone_number)==12:
+        phone_number='+'+phone_number
+    return phone_number
 
-date = input("Введість дату в форматі yyyy-mm-dd: ")           
-result = get_days_from_today(date)
-print({result})
-    
+
+for phone in raw_numbers:
+    print(normalize_phone(phone))
