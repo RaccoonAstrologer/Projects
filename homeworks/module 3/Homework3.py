bff39fd749f18b806e6f2d2ffce54d123ff6bbe4
RaccoonAstrologer <z.niklal@gmail.com>
2024-01-26 10:55:31 +0200

    h3

--- homeworks/module 3/Homework3.py
+++ homeworks/module 3/Homework3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import re
 raw_phones = [
     "067\\t123 4567",
@@ -28,36 +27,3 @@ def normalize_phone(phone_number):
 
 for phone in raw_phones:
     print(normalize_phone(phone))
-    
-=======
-import re
-raw_phones = [
-    "067\\t123 4567",
-    "(095) 234-5678\\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",
-]
-dirty_phones = ["    +38(050)123-32-34",
-"     0503451234",
-"(050)8889900",
-"38050-111-22-22",
-"38050 111 22 11   "]
-def normalize_phone(phone_number):
-    p1=r"[\d\+]+"
-    phone_number=''.join(re.findall(p1,phone_number))
-    if len(phone_number)==10:
-        phone_number='+38'+phone_number
-    elif len(phone_number)==12:
-        phone_number='+'+phone_number
-    return phone_number
-
-
-for phone in raw_phones:
-    print(normalize_phone(phone))
->>>>>>> 19ebd52e030dc4f20f74ac31058ec51d2b8d47c9
-    
