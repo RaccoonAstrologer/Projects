8fd9bdc342cabcd4913eb29f278b5e0402c58c29
RaccoonAstrologer <z.niklal@gmail.com>
2024-01-27 14:31:55 +0200

    h2

--- homeworks/module 3/Homework2.py
+++ homeworks/module 3/Homework2.py
@@ -1,9 +1,11 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    if (max < min ):
+        return False
     for num in [min, max, quantity]:
         if not (1 <= num <= 1000):
-            return []
+            return False
         uniq_numbs = random.sample(range(min, max + 1), quantity)
         return sorted(uniq_numbs)
 lottery_numbers = get_numbers_ticket(1,49,6)
